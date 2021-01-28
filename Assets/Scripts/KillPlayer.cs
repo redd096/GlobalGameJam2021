df967e0ab97c255b0e076724b79602a9736832de
redd096 <martin.babino96@gmail.com>
2021-01-28 23:29:43 +0100

    on trigger enter kill player

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponentInParent<Player>();
+        if(player)
+        {
+            Destroy(player.gameObject);
+        }
+    }
+}
