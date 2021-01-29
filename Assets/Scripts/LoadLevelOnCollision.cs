dd1ed42aee2254e75b1eb1cdb202b86b3604a9b0
redd096 <martin.babino96@gmail.com>
2021-01-29 17:46:34 +0100

    creato script LoadLevelOnCollision, funziona sia trigger che collider

---
+++ Assets/Scripts/LoadLevelOnCollision.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using redd096;
+
+[AddComponentMenu("Global Game Jam 2021/Load Level On Collision")]
+public class LoadLevelOnCollision : MonoBehaviour
+{
+    [Header("Load Level")]
+    [SerializeField] string nameLevelToLoad = "NameLevel";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if hit player
+        if(collision.GetComponentInParent<Player>())
+        {
+            LoadLevel();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if hit player
+        if (collision.gameObject.GetComponentInParent<Player>())
+        {
+            LoadLevel();
+        }
+    }
+
+    void LoadLevel()
+    {
+        //load scene
+        SceneLoader.instance.LoadNewScene(nameLevelToLoad);
+    }
+}
