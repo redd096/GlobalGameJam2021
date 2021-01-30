e41d849b1de84f422d3dbd83b112988257d741fb
redd096 <martin.babino96@gmail.com>
2021-01-30 18:22:57 +0100

    nella slider volume e nei toggle, trascinare il prefab del Game Manager e usare i comandi in OptionsManager

---
+++ Assets/Scripts/OptionsManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using redd096;
+
+[AddComponentMenu("Global Game Jam 2021/Options Manager")]
+public class OptionsManager : Singleton<OptionsManager>
+{
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+
+    public void SetUseMouse(bool useMouse)
+    {
+
+    }
+
+    public void SetPostProcess(bool usePostProcess)
+    {
+
+    }
+}
