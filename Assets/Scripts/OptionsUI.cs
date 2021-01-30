32b367f6c7af85ed8ca72bf001eea62a3c26789f
redd096 <martin.babino96@gmail.com>
2021-01-30 19:02:44 +0100

    aggiornato opzioni

---
+++ Assets/Scripts/OptionsUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsUI : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] Slider sliderVolume = default;
+    [SerializeField] Toggle toggleUseAim = default;
+    [SerializeField] Toggle toggleUsePostProcessLayer = default;
+
+    void Start()
+    {
+        //update UI
+        sliderVolume.value = OptionsManager.instance.volume;
+        toggleUseAim.isOn = OptionsManager.instance.useAim;
+        toggleUsePostProcessLayer.isOn = OptionsManager.instance.usePostProcessLayer;
+    }
+}
