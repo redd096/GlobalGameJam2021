feb193df4e4a0d7128532b43c094cfd71cb1e432
redd096 <martin.babino96@gmail.com>
2021-01-31 01:19:54 +0100

    pushati cursori randomizzati

---
+++ Assets/Scripts/RandomizeCursor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using redd096;
+
+[AddComponentMenu("Global Game Jam 2021/Randomize Cursor")]
+public class RandomizeCursor : Singleton<RandomizeCursor>
+{
+    [Header("Cursors")]
+    [SerializeField] Texture2D[] cursors = default;
+
+    [Header("Golden Cursor")]
+    [SerializeField] Texture2D goldenCursor = default;
+    [Range(0, 1)] [SerializeField] float percentageGolden = 0.01f;
+
+    protected override void SetDefaults()
+    {
+        base.SetDefaults();
+
+        Texture2D sprite;
+
+        float random = Random.value;
+        //if percentage, take golden (only if there is golden)
+        if (random <= percentageGolden && goldenCursor != null)
+        {
+            sprite = goldenCursor;
+        }
+        //else random head
+        else
+        {
+            sprite = cursors[Random.Range(0, cursors.Length)];
+        }
+
+        //change sprite if there is one
+        Cursor.SetCursor(sprite, Vector2.zero, CursorMode.Auto);
+    }
+}
